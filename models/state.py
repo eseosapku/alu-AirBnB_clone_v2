2c6e149fd49fd3750d420d83449f45ef15cadad0
dn004 <d.ntwari@alustudent.com>
2023-02-24 01:29:20 -0800

    rw

--- models/state.py
+++ models/state.py
@@ -8,13 +8,15 @@ from sqlalchemy.orm import relationship
 from models.city import City
 import models
 
+
 class State(BaseModel, Base):
     """ State class """
+
     __tablename__ = 'states'
     
+
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',cascade='all, delete-orphan')
-
     
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property
