e05a3059d586387580d3cd1b6be54887a3296501
dn004 <d.ntwari@alustudent.com>
2023-03-09 12:20:16 +0000

    kk

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-""" Module """
-
-from datetime import datetime
+"""
+Module
+"""
+from time import strftime
 from fabric.api import local
 
+
 def do_pack():
-#	generate into .tgz archive
-	try:
-	    #archives stored in folder
-	    local ("mkdir -p versions")
-	    #name of archive must be created
-	    archive_name = "versions/web_static_{}.tgz".format(strftime("%Y%M%d%H%M%S")
-	    #create archive	    
-	    local("tar -cvzf {} web_static".format(archive_name))
+    """
+    generates a .tgz archive from the contents of the web_static folder of
+    your AirBnB Clone repo
+    """
+    try:
+        # All archives must be stored in the folder versions
+        # (your function should create this folder if it doesn’t exist)
+        local("mkdir -p versions")
 
-	    return archive_name
-	except Exception
-	    return None
+        # The name of the archive created must be
+        # web_static_<year><month><day><hour><minute><second>.tgz
+        archive_name = "versions/web_static_{}.tgz".format(
+            strftime("%Y%M%d%H%M%S"))
 
-	
+        # Create archive
+        # All files in the folder web_static must be added to the final archive
+        local("tar -cvzf {} web_static/".format(archive_name))
+
+        # return the archive path
+        return archive_name
+    except Exception:
+        return None
