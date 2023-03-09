072bffe66b0b06fb6636c3d9fd4463d6fbad3897
dn004 <d.ntwari@alustudent.com>
2023-03-09 12:00:04 +0000

    joie de vivre

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script creates and distributes an archive to your web servers"""
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+def deploy():
+    """function creates and distributes an archive to web servers"""
+    from fabric.api import env
+    env.hosts = ['ip_web-01', 'ip_web-02']  # list of web servers
+    archive_path = do_pack()
+    if archive_path is None:  # if archive_path is None:
+        return False
+    return do_deploy(archive_path)  # deploy = do_pack() + do_deploy()
