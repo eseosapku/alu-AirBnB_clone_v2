6d647dbed8eea371e9adeced3a86afa9e430fec7
dn004 <d.ntwari@alustudent.com>
2023-03-09 11:32:07 +0000

    spell

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module """
+
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+	""" into .tgz archive"""
+	try:
+	    local ("mkdir -p versions")
+	    archive_name = "versions/web_static_{}.tgz".format(strftime("%Y%M%d%H%M%S")
+	    result = local("tar -cvzf {} web_static".format(doc_name))
+
+	    return doc_name
+	except Exception
+	    return None
+
+	
