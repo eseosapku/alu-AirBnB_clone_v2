5a4f3a866ef8291c2d0a0983368abc7cee1a7fca
eseosapku <e.uwagboe@alustudent.com>
2023-03-10 20:15:16 +0000

    comit

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+"""
+
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
