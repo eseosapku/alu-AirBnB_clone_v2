e607be8211d93910d5601f5325ef75882907ecc9
eseosapku <e.uwagboe@alustudent.com>
2023-03-10 20:20:03 +0000

    comit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,3 +8,15 @@ from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
 env.hosts = ['3.91.98.157', '54.164.137.93']
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
