1ded4a69b6b80526a207cbf082ee530e5a3b312b
eseosapku <e.uwagboe@alustudent.com>
2023-03-10 20:17:51 +0000

    comit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,3 +7,4 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
+env.hosts = ['3.91.98.157', '54.164.137.93']
