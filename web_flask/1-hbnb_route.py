d645289263a3596760ae4c1ac19c7c45ecb5fd25
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 18:23:48 +0000

    comit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
 # """returns hello HBNB!"""
-    return "Hello HBNB!"
+return "Hello HBNB!"
 
 if __name__ == "__main__":
 app.run(host="0.0.0.0", port=5000, debug=True)
