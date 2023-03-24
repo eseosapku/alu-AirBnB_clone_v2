99133067c4e3372e0c7700c5f056f7faa35f3b5d
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 19:21:41 +0000

    comit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application 
-"""
+""" starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
@@ -22,7 +20,5 @@ def close_session(self):
     End session after request 
     """
     storage.close()
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
