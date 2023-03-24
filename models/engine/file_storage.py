c140030c6977119f81afb5a5282003726b82553c
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 19:15:32 +0000

    comit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,7 +63,6 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-     
-    def close(self):
+def close(self):
         """Deserialize the JSON file to objects"""
         self.reload()
