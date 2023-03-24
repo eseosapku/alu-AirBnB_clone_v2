4ad11c35bb58c203482123acae9ce62ab8195e40
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 19:01:26 +0000

    comit

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,59 +1,53 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
+""" working with default routes
+    python is cool"""
 from flask import Flask
+from flask import render_template
+from markupsafe import Markup
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    return Hello HBNB!
-    """
+@app.route("/")
+def home():
+    """ default home route """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
-    """
-    return HBNB
-    """
+    """  hbnb end point """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """
-    return C followed by the text variable
-    """
-    return "C {}".format(text.replace('_', ' '))
+@app.route("/c/<text>")
+def info_c(text):
+    """ taking argument for displaying c """
+    takeout = text.replace("_", " ")
+    return "C {}".format(takeout)
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    """
-    return Python followed by the text variable
-    """
-    if text is not 'is cool':
-        text = text.replace('_', ' ')
-    return "Python {}".format(text.replace('_', ' '))
+@app.route('/python/')
+@app.route('/python/<text>')
+def pythonR(text='is cool'):
+    """ python is cool """
+    takeout = text.replace("_", " ")
+    return "Python {}".format(takeout)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def number(n):
-    """
-    return n is a number if n is an integer
-    """
+    """ work for numbers only """
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """
-    return HTML page if n is an integer
-    """
-    return render_template('5-number.html', number=n)
+@app.route('/number_template/<int:n>')
+def templateNumber(n):
+    """ display template if only integer """
+    n = Markup.escape(n)
+    return render_template('5-number.html', n=n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # """ everything starts here """
     app.run(host="0.0.0.0", port=5000, debug=True)
