e9792658ad422db7de6d231c2f6cfd1092cd3564
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 17:49:49 +0000

    commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Start Flask web application
-"""
-
+"""Start Flask web application"""
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
