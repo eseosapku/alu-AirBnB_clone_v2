49bdd141d78d9d50e8830da3004138f569bbe914
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 17:09:15 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,12 @@ Script that starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    returns hello HBNB!
-    """
+	"""
+	returns hello HBNB!
+	"""
 	return "Hello HBNB!"
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
