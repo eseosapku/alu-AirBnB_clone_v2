0fba29086f696954db41477de2fffa889f9ccd36
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 17:19:15 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 def hello_hbnb():
 	"""
 returns hello HBNB!
-	"""
-	return "Hello HBNB!"
+"""
+return "Hello HBNB!"
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
