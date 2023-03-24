c269964708cd8cbec8ea9582276ab2385b360a75
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 19:31:07 +0000

    commit

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application 
-"""
+""" starts a Flask web application """
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities():
