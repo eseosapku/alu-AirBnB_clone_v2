c28a05c4d757c51f9486fb8c8f25fc412aca5922
eseosapku <e.uwagboe@alustudent.com>
2023-03-24 19:24:21 +0000

    comit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,11 +14,10 @@ def states_list():
     return render_template('7-states_list.html',
                            states=storage.all('State').values())
 
+
 @app.teardown_appcontext
 def close_session(self):
-    """
-    End session after request 
-    """
+    """ End session after request """
     storage.close()
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
